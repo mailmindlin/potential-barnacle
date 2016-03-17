5f82a025974a8514b26ca8fda2beead8e9a46a49
mailmindlin <liamttlyrox@gmail.com>
2016-03-17 15:01:46 -0400

    More UI update

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('status.html', {
+	chrome.app.window.create('ui/status.html', {
 		'outerBounds': {
 			'width': 400,
 			'height': 500
---
+++ ui/ui.js
@@ -0,0 +1,11 @@
+function loadGames() {
+	
+}
+onload = function() {
+	var tcpServer = chrome.sockets.tcpServer;
+	var tcpSocket = chrome.sockets.tcp;
+	var udpSocket = chrome.sockets.udp;
+	
+	var viewStart = $('#view-start');
+	
+};
