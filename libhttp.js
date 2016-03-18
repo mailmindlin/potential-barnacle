a7993f3706823091a1fab145d8247f0d54546ef5
mailmindlin <liamttlyrox@gmail.com>
2016-03-18 15:02:22 -0400

    More HTTP work

--- libhttp.js
+++ libhttp.js
@@ -1,6 +1,22 @@
 var HTTP_NEWLINE = '\r\n';
 var tcpServer = chrome.sockets.tcpServer;
 var tcpSocket = chrome.sockets.tcp;
+var stringToUint8Array = function(string) {
+	var buffer = new ArrayBuffer(string.length);
+		var view = new Uint8Array(buffer);
+		for (var i = 0; i < string.length; i++)
+			view[i] = string.charCodeAt(i);
+	return view;
+};
+
+var arrayBufferToString = function(buffer) {
+	var str = '';
+	var uArrayVal = new Uint8Array(buffer);
+	for (var s = 0; s < uArrayVal.length; s++)
+		str += String.fromCharCode(uArrayVal[s]);
+	return str;
+};
+
 class HttpServer {
 	constructor(port) {
 		this._port = port;
@@ -28,17 +44,17 @@ class HttpServer {
 		});
 	}
 	_accept(socket) {
-		var id = socket.clientSocketId;
-		tcpSocket.setPaused(id, false);
-		if (id != this.socket.socketId) {
-			console.error('Invalid socket id ' + id + ' (expected ' + this.socket.socketId + ')');
+		tcpSocket.setPaused(socket.clientSocketId, false);
+		if (socket.socketId != this.socket.socketId) {
+			console.error('Invalid socket id ' + socket.socketId + ' (expected ' + this.socket.socketId + ')');
 			return;
 		}
 		console.info('ACCEPT', socket);
 	}
 	_read(socket) {
 		console.info('READ', socket);
-		
+		var data = arrayBufferToString(socket.data);
+		console.log(data);
 	}
 	set paused(flag) {
 		return doPromise(tcpServer.setPaused, tcpServer, flag);
