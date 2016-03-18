6a1ed5c6e8756fb92ac29bba539065a6ed97135d
mailmindlin <mailmindlin@users.noreply.github.com>
2016-03-18 08:31:29 -0400

    Update ui.js

--- ui/ui.js
+++ ui/ui.js
@@ -1,5 +1,11 @@
-function loadGames() {
-	
+function updateSavedGames() {
+	Storage.get('games').then(games => {
+		if (games) {
+			var list = $('#saved-games').empty();
+			for (let game of games)
+				list.append($(ce('li')).html(game));
+		}
+	});
 }
 onload = function() {
 	var tcpServer = chrome.sockets.tcpServer;
@@ -7,5 +13,5 @@ onload = function() {
 	var udpSocket = chrome.sockets.udp;
 	
 	var viewStart = $('#view-start');
-	
+	updateSavedGames();
 };
