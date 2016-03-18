87915ce157f2417636fdc427a8060a80bb06b682
mailmindlin <mailmindlin@users.noreply.github.com>
2016-03-18 08:31:48 -0400

    Create util.js

---
+++ ui/util.js
@@ -0,0 +1,16 @@
+function doPromise(fn, self) {
+	var args = Array.prototype.slice.call(arguments, 2);
+	return new Promise((yay, nay)=>{
+		args.push(function(e){chrome.runtime.lastError?nay(chrome.runtime.lastError, e):yay(e);});
+		fn.apply(self, args);
+	});
+}
+window.ce=(e)=>(document.createElement(e));
+var Storage = {
+	ls: ()=>(doPromise(chrome.storage.sync.get, chrome.storage.sync, null)),
+	getAll: files=>(doPromise(chrome.storage.sync.get, chrome.storage.sync, files)),
+	get: file=>(Storage.getAll(file).then((e)=>(e[file]))),
+	put: data=>(doPromise(chrome.storage.sync.set, chrome.storage.sync, data)),
+	rm: file=>(doPromise(chrome.storage.sync.remove, chrome.storage.sync, file)),
+	clear: file=>(doPromise(chrome.storage.sync.clear, chrome.storage.sync))
+};
