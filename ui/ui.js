e3e28b9def3d312606a1abdcf362c04e5aed96fa
mailmindlin <liamttlyrox@gmail.com>
2016-04-08 14:59:18 -0400

    Worked on input

--- ui/ui.js
+++ ui/ui.js
@@ -1,3 +1,4 @@
+var currentGame;
 function showStartScreen() {
 	Storage.get('games').then(games => {
 		var gameList = $('#saved-games').empty();
@@ -7,9 +8,6 @@ function showStartScreen() {
 		$('.view-active:not(#view-start)').removeClass('view-active');
 		View.transitionTo('start');
 	});
-}
-function createGame() {
-	
 }
 function loadGame(name) {
 	Storage.get('games').then(games=> {
@@ -26,14 +24,45 @@ onload = function() {
 	
 	$('#view-start .view-footer').click(() => {
 		View.transitionTo('create-game-0');
+		currentGame = {};
 		$('#view-create-game-0 input').focus();
 	});
 	
+	$('#view-create-game-0 input').on('keypress', e => {
+		var key = e.keyCode || e.which;
+		if (key == 9) { 
+			e.preventDefault(); 
+			$('#view-create-game-0 button').focus();
+		} 
+	});
+	
 	$('#view-create-game-0 button').click(() => {
-		if ($('#view-create-game-0 input').val() == '')
+		var name = $('#view-create-game-0 input').val();
+		if (name == '')
 			return;
+		currentGame.name = name;
 		View.transitionTo('create-categories');
 		$('#view-create-categories input:first-of-type').val('').focus();
+	});
+	
+	$('#view-create-categories .button-continue').click(e => {
+		currentGame.categories = [];
+		var table = $('#view-create-values table');
+		var thead = table.find('thead tr').empty();
+		var tbody = table.find('tbody').empty();
+		for (var category of $('#category-inputs').children()) {
+			var name = $(category).val();
+			console.log(name);
+			currentGame.categories.push(name);
+			thead.append($('<th>').html(name));
+		}
+		for (var i = 1; i <= 5; i++) {
+			var row = $('<tr>');
+			for (var j of currentGame.categories)
+				row.append($('<td>').html(i + '00'));
+			tbody.append(row);
+		}
+		View.transitionTo('create-values');
 	});
 	showStartScreen();
 };
